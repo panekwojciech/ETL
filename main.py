f33100cc4c278e82dc229a8099bdba7ef5170646
Mistblinde <pawelboka@interia.pl>
2017-11-11 13:11:59 +0100

    Pokazuje uzytkownika i czy poleca czy nie

--- main.py
+++ main.py
@@ -18,15 +18,11 @@ def get_product_opinion(product_number):
 	soup = BeautifulSoup(mystr, 'html.parser')
 
 	for paragraph_nickname in soup.find_all("div", class_="reviewer-name-line"):
-		nicknames = paragraph_nickname.string
-		print(nicknames)
-		for	paragraph_recommend in soup.find_all("em", class_="product-recommended"):
+		for	paragraph_recommend in soup.find("em", class_="product-recommended"):
+			nicknames = paragraph_nickname.string
+			print(nicknames)
 			recommend = paragraph_recommend.string
 			print(recommend)
-		for	paragraph_not_recommend in soup.find_all("em", class_="product-not-recommended"):
-			not_recommend = paragraph_not_recommend.string
-			print(not_recommend)
-
 
 
 get_product_opinion(51499287)
