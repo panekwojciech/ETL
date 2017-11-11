3f339adcc2eae0b9ddcb22e395bb1e8200a3357d
Mistblinde <pawelboka@interia.pl>
2017-11-11 15:00:43 +0100

    pozmieniane

--- main.py
+++ main.py
@@ -4,18 +4,28 @@ import urllib.request
 import ssl
 from bs4 import BeautifulSoup
 
+
 def get_product_opinion(product_number):
 	context = ssl._create_unverified_context()
 	fp = urllib.request.urlopen('https://www.ceneo.pl/' + str(product_number) + '#tab=reviews', context=context).read()
 	mystr = fp.decode("utf8")
 	soup = BeautifulSoup(mystr, 'html.parser')
 
-	for paragraph_nickname in soup.find_all("div", class_="reviewer-name-line"):
-		for	paragraph_recommend in soup.find("em", class_="product-recommended"):
+	reviewer_name = soup.find_all("div", class_="reviewer-name-line")
+	product_recommended = soup.find("em", class_="product-recommended")
+	product_recommended_string = product_recommended.string
+	product_not_recommended = soup.find("em", class_="product-not-recommended")
+	product_not_recommended_string = product_not_recommended.string
+
+	for paragraph_nickname in reviewer_name:
+		if product_recommended_string == 'Polecam':
 			nicknames = paragraph_nickname.string
 			print(nicknames)
-			recommend = paragraph_recommend.string
-			print(recommend)
+			print('Polecamm')
+		elif product_not_recommended_string == 'Nie polecam':
+			nicknames = paragraph_nickname.string
+			print(nicknames)
+			print('Nie polecam')
 
 
-get_product_opinion(52351465)
+get_product_opinion(51499287)
